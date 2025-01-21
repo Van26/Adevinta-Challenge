e95eeeb259373bb196e11305b110435e9c7bae1d
Adrián Diaz Rodríguez <65287849+AdrianDR26@users.noreply.github.com>
2025-01-21 23:35:38 +0100

    added AsyncImage

--- designsystem/src/main/java/cmm/apps/designsystem/AdevintaCardInfo.kt
+++ designsystem/src/main/java/cmm/apps/designsystem/AdevintaCardInfo.kt
@@ -1,13 +1,12 @@
 package cmm.apps.designsystem
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -16,9 +15,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 
 @Composable
 fun AdevintaCardInfo(
@@ -42,12 +41,11 @@ fun AdevintaCardInfo(
             modifier = Modifier.padding(16.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.outline_cancel), contentDescription = "App logo",
-                modifier = Modifier
-                    .fillMaxWidth(0.3f)
-                    .aspectRatio(1f)
-                    .clip(RoundedCornerShape(32.dp))
+            AsyncImage(
+                modifier = Modifier.size(70.dp),
+                contentScale = ContentScale.Crop,
+                model = model.imageUrl ?: "",
+                contentDescription = "Translated description of what the image contains",
             )
             Spacer(modifier = Modifier.width(8.dp))
             Column {

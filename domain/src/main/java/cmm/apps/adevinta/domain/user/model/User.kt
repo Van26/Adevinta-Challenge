5af5f99e4c4cdc878e22b53526560bb069575cf4
Adrián Diaz Rodríguez <65287849+AdrianDR26@users.noreply.github.com>
2025-01-22 18:24:02 +0100

    improve full address parse

--- domain/src/main/java/cmm/apps/adevinta/domain/user/model/User.kt
+++ domain/src/main/java/cmm/apps/adevinta/domain/user/model/User.kt
@@ -26,12 +26,11 @@ data class Location(
     val country: String?
 ) {
     fun getFullAddress(): String {
-        val streetBody1 = streetName?.let { streetNumber?.let { "$streetName $it" } ?: "$streetName" } ?: ""
-        val streetBody2 = city?.let { if (streetBody1.isBlank()) ", $it" else it } ?: ""
-        val streetBody3 = state?.let { if ((streetBody1 + streetBody2).isBlank()) ", $it" else it } ?: ""
-        val streetBody4 =  postcode?.let { if ((streetBody1 + streetBody2 + streetBody3).isBlank()) ", $it" else it } ?: ""
-        val streetBody5 =  country?.let { if ((streetBody1 + streetBody2 + streetBody3 + streetBody4).isBlank()) ", $it" else it } ?: ""
-
-        return streetBody1 + streetBody2 + streetBody3 + streetBody4 + streetBody5
+        return formatNullableStrings("$streetName $streetNumber".trim(), city, state, postcode, country)
     }
+
+    private fun formatNullableStrings(vararg strings: String?): String {
+        return strings.filter { !it.isNullOrBlank() }.joinToString(", ")
+    }
+
 }
